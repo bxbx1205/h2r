4597714e7ad855ad65e731848f1b92dbd3866f96
Anurag <anuragsharma5617@gmail.com>
2025-05-18 00:13:26 +0530

    landing page final

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PixelTransition from "./PixelTransition";
+import GradientText from "./GradientText";
 
 export default function LandingPage() {
     return (
@@ -15,26 +16,34 @@ export default function LandingPage() {
                     className="bg-gray-800 text-white px-4 py-2 rounded-full text-sm hover:bg-gray-700 transition"
                 >
                     RBU's only social space to exist
-                </a>            </div>
+                </a>
+            </div>
 
             {/* Navigation Links */}
             <nav className="flex space-x-8 mt-2">
-                <a href="#" className="text-gray-400 hover:text-white transition">NGL</a>
                 <a href="#" className="text-gray-400 hover:text-white transition">Discord</a>
                 <a href="#" className="text-gray-400 hover:text-white transition">Instagram</a>
+                <a href="#" className="text-gray-400 hover:text-white transition">Anonymous Confession</a>
                 <a href="#" className="text-gray-400 hover:text-white transition">Notes Warehouse</a>
             </nav>
 
             {/* Main Content */}
             <main className="flex flex-col items-center justify flex-1 mt-24">
-                <h1 className="text-4xl md:text-5xl text-gray-300 font-light mb-2">
-                    We're just getting <span className="text-white font-large bold">started !!</span>
-                </h1>
+
+                <GradientText
+                    colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+                    animationSpeed={3}
+                    showBorder={false}
+                    className="custom-class text-2xl"
+                >
+                    We are bringing Anonymous Confessions , Notes Wareshouse and many more ...
+                </GradientText>
+
                 {/* Centered "dip" Section */}
                 <div className="flex flex-col items-center">
-                    <span className="text-2xl text-white font-normal mb-2">Anonymous Confessions, Notes Wareshouse and many more ...</span>
+                    <span className="text-xl text-white font-large mb-2">humansofrbu is a student driven community, working in shadows to create a social space for RBU students</span>
                     <span className="text-gray-400 text-lg mb-8">
-                        <span className="text-gray-600 text-sm mr-4">(hover it)</span>
+                        <span className="text-gray-600 text-xl mr-4">(hover it)</span>
                     </span>
                 </div>
             </main>
@@ -43,7 +52,7 @@ export default function LandingPage() {
             <PixelTransition
                 firstContent={
                     <img
-                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg"
+                        src="https://i.postimg.cc/Jh2kLW9y/Frame-45.png"
                         alt="default pixel transition content, a cat!"
                         style={{ width: "100%", height: "100%", objectFit: "cover" }}
                     />
@@ -58,13 +67,13 @@ export default function LandingPage() {
                             backgroundColor: "#111"
                         }}
                     >
-                        <p style={{ fontWeight: 900, fontSize: "3rem", color: "#ffffff" }}>Meow!</p>
+                        <p style={{ fontWeight: 900, fontSize: "3rem", color: "#ffffff" }}>We are cooking something !!</p>
                     </div>
                 }
                 gridSize={12}
                 pixelColor='#ffffff'
                 animationStepDuration={0.4}
-                className="custom-pixel-card w-full h-full"
+                className="custom-pixel-card w-7xl h-screen"
             />
         </div>
     );
