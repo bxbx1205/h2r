a06b6c602f64ff819b36f47792830d137ce8d5e5
bxbx1205 <srbhanarkar05@gmail.com>
2025-05-17 13:51:18 +0530

    modified:   app/page.jsx 	new file:   components/Squares.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,22 @@
-import Image from "next/image";
+import Squares from '../components/Squares.jsx';
 
 export default function Home() {
   return (
-    <>
-  <h1></h1>
-    
-    </>
-    
+    <div style={{
+      position: 'fixed',
+      inset: 0,
+      zIndex: -1, // behind other content
+      width: '100vw',
+      height: '100vh',
+      overflow: 'hidden'
+    }}>
+      <Squares 
+        speed={0.5} 
+        squareSize={40}
+        direction='down'
+        borderColor='#fff'
+        hoverFillColor='#ffa500'
+      />
+    </div>
   );
 }
