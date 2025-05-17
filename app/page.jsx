e62f21884c5b4afd06c4aed8c58534efb5bfb70f
Anurag <anuragsharma5617@gmail.com>
2025-05-17 15:46:24 +0530

    Landing Page

--- app/page.jsx
+++ app/page.jsx
@@ -1,32 +1,33 @@
 "use client";
 import Squares from '../components/Squares.jsx';
 import DiscordLanding from '../components/DiscordLanding.jsx';
+import LandingPage from '../components/LandingPage.jsx';
 
 
 export default function Home() {
-  return (
+    return (
+        <>
+            <div className="app">
+                <LandingPage />
+                <DiscordLanding />
+            </div>
 
-    <>
-    <div className="app">
-      <DiscordLanding />
-    </div>
-
-    <div style={{
-      position: 'fixed',
-      inset: 0,
-      zIndex: -1, // behind other content
-      width: '100vw',
-      height: '100vh',
-      overflow: 'hidden'
-    }}>
-      <Squares 
-        speed={0.5} 
-        squareSize={40}
-        direction='down'
-        borderColor='#fff'
-        hoverFillColor='#ffa500'
-      />
-    </div>
-    </>
-  );
+            <div style={{
+                position: 'fixed',
+                inset: 0,
+                zIndex: -1, // behind other content
+                width: '100vw',
+                height: '100vh',
+                overflow: 'hidden'
+            }}>
+                <Squares
+                    speed={0.5}
+                    squareSize={40}
+                    direction='down'
+                    borderColor='#fff'
+                    hoverFillColor='#ffa500'
+                />
+            </div>
+        </>
+    );
 }
